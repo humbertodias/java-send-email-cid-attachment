6cb8bf505ac59b325916878743a101dbf1eb3295
Humberto Dias <hldtux@gmail.com>
2016-06-24 02:00:21 -0300

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# java-send-mailer
-Java Send Mailer
+# Java Send Mailer
+
+Java Tools for generating QRCode.
+
+
+## Prerequires
+
+1. Git 2.6+
+2. Maven 3.3+
+3. Java 8+
+
+
+## How to Play
+
+Clone
+
+```
+git clone https://github.com/humbertodias/java-send-mailer.git
+```
+
+Inside the folder
+
+```
+cd java-send-mailer
+```
+
+**Package**
+
+```
+mvn clean package
+```
+
+**Send**
+
+```
+java -jar target/send-mail-0.0.1-SNAPSHOT.jar \
+-from from@gmail.com \
+-to mail1@gmail.com,mail2@yahoo.com.br \
+-replyto noreply@gmail.com \
+-subject SUBJECT \
+-smtp.host smtp.gmail.com \
+-smtp.port 587  \
+-html "<html><body><img src='cid:java-logo.png'/></body></html>" \
+-attachs doc/java-logo.png \
+-user USERNAME \
+-password
+
+```
+
+**Arguments**
+
+Parameter | Description
+------------- | -------------
+-from | From Address
+-to | To Address
+-replyto | Reply To Address
+-subject |  Subject
+-smtp.host | SMTP Host
+-smtp.port | SMTP Port
+-html | HTML Message
+-attachs | Attachment
+-user | User
+-password | Password
+
+## Output
+
+![Preview](doc/package.png)
+
+
+## Recieved
+
+![Preview](doc/inbox.png)
+
+
+## References
+
+![Preview](doc/gmail-less-security.png)
+
+1. [GMail Less Security](https://support.google.com/accounts/answer/6010255?hl=en)
+
---
+++ src/main/java/util/SendEmail.java
@@ -0,0 +1,133 @@
+package util;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+public class SendEmail {
+
+	@Parameter(names = { "-h", "--help" }, help = true)
+	private boolean help;
+
+	@Parameter(names = "-v", description = "Version", required = false)
+	boolean version;
+
+	@Parameter(names = "-user", description = "User", required = true)
+	String authuser;
+
+	@Parameter(names = "-password", description = "Password", required = true, password = true)
+	String authpwd;
+
+	@Parameter(names = "-smtp.host", description = "SMTP Host", required = true)
+	String smtpHostName;
+
+	@Parameter(names = "-smtp.port", description = "SMTP Port", required = false)
+	Integer smtpPort = 587;
+
+	@Parameter(names = "-from", description = "From", required = true)
+	String from;
+
+	@Parameter(names = "-to", description = "To", required = true)
+	String to;
+
+	@Parameter(names = "-replyto", description = "Reply To", required = false)
+	String replyTo;
+
+	@Parameter(names = "-cc", description = "CC", required = false)
+	String cc;
+
+	@Parameter(names = "-bcc", description = "BCC", required = false)
+	String bcc;
+
+	@Parameter(names = "-subject", description = "Subject", required = true)
+	String subject;
+
+	@Parameter(names = "-html", description = "Html Message", required = true)
+	String htmlMessage;
+
+	@Parameter(names = "-debug", description = "Debug Mode", required = false)
+	Boolean debug = true;
+
+	@Parameter(names = "-tls", description = "TLS Enabled", required = false)
+	Boolean tls = true;
+
+	@Parameter(names = "-attachs", description = "Attachments", required = false)
+	private List<String> attachments = new ArrayList<String>();
+
+	void send() throws MalformedURLException, EmailException {
+		HtmlEmail email = new HtmlEmail();
+		email.setSmtpPort(smtpPort);
+		email.setAuthentication(authuser, authpwd);
+		email.setDebug(debug);
+		email.setStartTLSEnabled(tls);
+
+		email.setHostName(smtpHostName);
+
+		email.setFrom(from);
+		
+		if (replyTo != null)
+			email.addReplyTo(replyTo);
+
+		if (cc != null)
+			email.addCc(cc.split(","));
+		if (bcc != null)
+			email.addBcc(bcc.split(","));
+
+		email.addTo(to.split(","));
+		
+		email.setSubject(subject);
+
+		for (String apath : attachments) {
+			File file = new File(apath);
+			URL url = file.toURI().toURL();
+
+			EmailAttachment attachment = new EmailAttachment();
+			attachment.setURL(url);
+			attachment.setName(file.getName());
+			String tagCid = "cid:" + file.getName();
+
+			if (htmlMessage.contains(tagCid)) {
+				String cid = email.embed(url, file.getName());
+				htmlMessage = htmlMessage.replaceAll(tagCid, "cid:" + cid);
+
+			} else {
+				attachment.setDisposition(EmailAttachment.ATTACHMENT);
+				email.attach(attachment);
+			}
+
+		}
+
+		// set the html message
+		email.setHtmlMsg(htmlMessage);
+
+		// send the email
+		email.send();
+	}
+
+	public static void main(String... args) throws MalformedURLException, EmailException {
+		SendEmail sendMail = new SendEmail();
+		try {
+
+			JCommander jCommander = new JCommander(sendMail, args);
+			if (sendMail.help)
+				jCommander.usage();
+
+			sendMail.send();
+
+		} catch (ParameterException e) {
+			System.err.println(e.getLocalizedMessage());
+			System.exit(1);
+		}
+
+	}
+}
